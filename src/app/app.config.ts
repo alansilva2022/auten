6458dfb6284324f6813f8b8871fa2613b6cda243
Alan Alves <alan.alves.silva.2022@gmail.com>
2024-02-22 10:48:57 -0300

    adicionando provideFirestore - importante para os servicos firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,18 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'; //importante colocar
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), 
               provideClientHydration(), 
-              importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"autent-5db11","appId":"1:1018635080099:web:236aeea9a1e6732949ca16","databaseURL":"https://autent-5db11-default-rtdb.firebaseio.com","storageBucket":"autent-5db11.appspot.com","apiKey":"AIzaSyAUKmMzSc-uBwwm-s3HZua6ptlQSyN6vIg","authDomain":"autent-5db11.firebaseapp.com","messagingSenderId":"1018635080099"}))), 
+              importProvidersFrom(
+                  provideFirebaseApp(() => 
+                    initializeApp({"projectId":"autent-5db11","appId":"1:1018635080099:web:236aeea9a1e6732949ca16","databaseURL":"https://autent-5db11-default-rtdb.firebaseio.com","storageBucket":"autent-5db11.appspot.com","apiKey":"AIzaSyAUKmMzSc-uBwwm-s3HZua6ptlQSyN6vIg","authDomain":"autent-5db11.firebaseapp.com","messagingSenderId":"1018635080099"})),
+                    provideFirestore(() => getFirestore()) //adicionaddo
+                    ), 
               importProvidersFrom(provideAuth(() => getAuth())), 
-              importProvidersFrom(provideDatabase(() => getDatabase()))]
+              importProvidersFrom(provideDatabase(() => getDatabase()))],
+             
 };
