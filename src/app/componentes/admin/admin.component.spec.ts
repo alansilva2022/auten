eefcc71435c7bfe3cbf56404fdc83fdfc0be7f0e
Poliana A. Pereira <86753067+PolianaR@users.noreply.github.com>
2024-04-16 22:51:08 +0000

    importarcomponent

--- src/app/componentes/admin/admin.component.spec.ts
+++ src/app/componentes/admin/admin.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
 describe('AdminComponent', () => {
