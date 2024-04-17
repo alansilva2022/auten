b27ed627899f6191d3dc7b1a51ac7359d0152701
Poliana A. Pereira <86753067+PolianaR@users.noreply.github.com>
2024-04-17 00:24:13 +0000

    importei CommonModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { AdminComponent } from './componentes/admin/admin.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { NaoautorizadoComponent } from './componentes/naoautorizado/naoautorizado.component';
 import { RegistrousuarioComponent } from './componentes/registrousuario/registrousuario.component';
-import { LoginComponent } from './componentes/login/login.component';
+//import { LoginComponent } from './componentes/login/login.component';//
+import { IndexComponent } from './componentes/index.adm/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,AdminComponent, HomeComponent, NaoautorizadoComponent, RegistrousuarioComponent, LoginComponent],
+  imports: [CommonModule, RouterOutlet,AdminComponent, HomeComponent, NaoautorizadoComponent, RegistrousuarioComponent, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/componentes/index.adm/index.component.ts
+++ src/app/componentes/index.adm/index.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'admin-index.admin',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
+})
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],      //Para usar *ngIf é preciso importar CommonModule
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss'
 })
 export class IndexComponent {
 
