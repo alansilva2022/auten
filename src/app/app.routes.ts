0adb1122b40d263d620889ea7756ab98d8bd49e0
Poliana A. Pereira <86753067+PolianaR@users.noreply.github.com>
2024-04-17 00:46:20 +0000

    app.routes.ts index.component linha 20

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,31 +17,31 @@ import { ConsultartrasacaoComponent } from './componentes/consultartrasacao/cons
 import { PesquisarlivroComponent } from './componentes/pesquisarlivro/pesquisarlivro.component';
 import { RelatoriolivroComponent } from './componentes/relatoriolivro/relatoriolivro.component';
 import { ComentarioComponent } from './componentes/comentario/comentario.component';
+import {IndexComponent} from './componentes/index.adm/index.component';
 
-
-const redirecionarParaLogin = () => redirectUnauthorizedTo(['login']);
+const redirecionarParaIndexComponent = () => redirectUnauthorizedTo(['admin']);
 const redirecionarParaHome = () => redirectLoggedInTo(['home']);
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'}, 
+    {path: '', redirectTo: 'admin', pathMatch: 'full'}, 
     {path: 'naoautorizado', component: NaoautorizadoComponent},
-    {path: 'home', component: HomeComponent, ...canActivate(redirecionarParaLogin)}, // só vai para página home de estiver logado
+    {path: 'home', component: HomeComponent, ...canActivate(redirecionarParaIndexComponent)}, // só vai para página home de estiver logado
     
-    { path: 'admin', component: AdminComponent, ...canActivate(redirecionarParaLogin), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] } },
+    { path: 'admin', component: AdminComponent, ...canActivate(redirecionarParaIndexComponent), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] } },
     //canActive para verificar se o utilizador atual tem permissão para acessar o componente
     //data --> dados adicionais
     //hasRoleGuard para a Route possa verificar se o utilizador pode acessar a este componente
-    {path: 'registro', component: RegistrousuarioComponent,...canActivate(redirecionarParaLogin), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
+    {path: 'registro', component: RegistrousuarioComponent,...canActivate(redirecionarParaIndexComponent), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
     {path: 'login', component: LoginComponent, ...canActivate(redirecionarParaHome)}, //se fizer login, redireciona para home
-    {path: 'cadastrarlivro', component: CadastrarLivroComponent, ...canActivate(redirecionarParaLogin), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
-    {path: 'transacao', component: TransacaoComponent, ...canActivate(redirecionarParaLogin), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
-    {path: 'reserva', component: ReservaComponent, ...canActivate(redirecionarParaLogin)},
-    {path: 'reservaporusuario', component: MinhasreservasComponent, ...canActivate(redirecionarParaLogin)},
-    {path: 'buscarusuario', component: ConsultausuarioComponent, ...canActivate(redirecionarParaLogin), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
-    {path: 'historicotransacoes', component: ConsultartrasacaoComponent, ...canActivate(redirecionarParaLogin), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
+    {path: 'cadastrarlivro', component: CadastrarLivroComponent, ...canActivate(redirecionarParaIndexComponent), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
+    {path: 'transacao', component: TransacaoComponent, ...canActivate(redirecionarParaIndexComponent), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
+    {path: 'reserva', component: ReservaComponent, ...canActivate(redirecionarParaIndexComponent)},
+    {path: 'reservaporusuario', component: MinhasreservasComponent, ...canActivate(redirecionarParaIndexComponent)},
+    {path: 'buscarusuario', component: ConsultausuarioComponent, ...canActivate(redirecionarParaIndexComponent), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
+    {path: 'historicotransacoes', component: ConsultartrasacaoComponent, ...canActivate(redirecionarParaIndexComponent), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
     {path: 'pesquisarlivro', component: PesquisarlivroComponent},
-    {path: 'relatoriolivro', component: RelatoriolivroComponent, ...canActivate(redirecionarParaLogin), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
-    {path: 'comentariolivro', component: ComentarioComponent, ...canActivate(redirecionarParaLogin)},
+    {path: 'relatoriolivro', component: RelatoriolivroComponent, ...canActivate(redirecionarParaIndexComponent), canActivate: [hasRoleGuard], data: { roles: [Role.Admin] }},
+    {path: 'comentariolivro', component: ComentarioComponent, ...canActivate(redirecionarParaIndexComponent)},
   
   
 ];
