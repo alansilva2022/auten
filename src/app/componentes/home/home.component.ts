76fabbed2384c0d2cb157f615b8f42841568d6d8
Poliana A. Pereira <86753067+PolianaR@users.noreply.github.com>
2024-04-30 20:04:17 +0000

    falta concluir home.component.ts

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../servicos/auth.service';
 import { CommonModule } from '@angular/common';
+import { Role } from '../../role';
 
 @Component({
   selector: 'app-home',
@@ -9,21 +10,56 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+ 
 export class HomeComponent {
 
- //antes estava como private, mas para o html ter acesso foi preciso colocar como public
-  constructor(public authService: AuthService){}
+    somenteAdm: boolean = false;
+    somenteUsuarioComum: boolean = false;
 
+    //antes estava como private, mas para o html ter acesso foi preciso colocar como public
+ constructor(public authService: AuthService){
+    this.authService.utilizadorAtual$.subscribe(user => {
+      console.log('Usuario atual:', user);
+      this.isAdmin();
+      this.isUsuarioComum();
+
+    });
+
+ }
   user$ = this.authService.utilizadorAtual$; //para ver o email logado
 
+}
+  isAdmin(): void {
+    this.authService.getUserRole().then(role => {
+      if (role == Role.Admin) {
+        //Usuário é admin, mostrar botão
+        this.somenteAdm =true;
+      } else {
+        // Usuário não é admin, não mostrar o botão
+        this.somenteAdm = false;
+      }
+      }
+    }).catch(error => {
+      console.error('Error ao verificar permissões', error);
+      //Em caso de erro, não mostrará o botão;
+      this.somenteAdm = false;
 
-
-  logout():void{
-    this.authService.logout();
+    });
+  
+  isUsuarioComum(): void {
+    this.authService.getUserRole().then(role =>{
+      if (role == Role.Admin) {
+         // Usuário não é adm, mostrar o botão 
+         this.somenteUsuarioComum = true;
+        } else {
+          // Usuário é adm, não mostrar o botão
+          this.somenteUsuarioComum = false;
+        }
+      }).catch(error => {
+        console.error('Erro ao verificar permissões:', error);
+        // Em caso de erro, não mostrar o botão
+        this.somenteUsuarioComum = false;
+      });
+    }
 
   }
-
-  
-
-
-}
