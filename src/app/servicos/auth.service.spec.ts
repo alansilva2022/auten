224b108b061b97dcddf20cce89ce5a5a01dbd8a1
Alan Alves <alan.alves.silva.2022@gmail.com>
2024-09-29 21:42:09 -0300

    Alguns testes em auth service e livro service

--- src/app/servicos/auth.service.spec.ts
+++ src/app/servicos/auth.service.spec.ts
@@ -1,16 +1,147 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
+import { Firestore} from '@angular/fire/firestore';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { Usuario } from '../componentes/usuario';
+import { Funcao } from '../funcao';
+import { Auth, getAuth, provideAuth } from '@angular/fire/auth';
+
 
 describe('AuthService', () => {
   let service: AuthService;
+  let firestore: Firestore;
+  let auth: any;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+   await TestBed.configureTestingModule({
+    imports: [
+      provideFirebaseApp(() => initializeApp({
+        apiKey: 'AIzaSyAUKmMzSc-uBwwm-s3HZua6ptlQSyN6vIg',
+        authDomain: 'autent-5db11.firebaseapp.com',
+        databaseURL: 'https://autent-5db11-default-rtdb.firebaseio.com',
+        projectId: 'autent-5db11',
+        storageBucket: 'autent-5db11.appspot.com',
+        messagingSenderId: '1018635080099',
+      })),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth())
+    ],
+    providers: [
+      AuthService
+    ]
+      
+    });
     service = TestBed.inject(AuthService);
+    firestore = TestBed.inject(Firestore);
+    auth = TestBed.inject(Auth);
   });
 
-  it('should be created', () => {
+
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
   });
+ 
+
+  it('deve fazer login com sucesso', async () => {
+    const email = 'teste@teste.com';
+    const password = 'teste123';
+    try {
+      await service.login(email, password);
+      expect(service.utilizadorAtual$).toBeTruthy();
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
+  });
+
+
+  
+
+  it('deve falhar ao fazer login', async () => {
+    const email = 'teste@teste.com';
+    const password = 'teste1234';
+    try {
+      await service.login(email, password);
+      fail('Deveria ter falhado');
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+
+ 
+
+  it('deve falhar ao registrar um novo usuário', async () => {
+    const email = 'teste@teste.com';
+    const password = 'teste1234';
+    const usuario: Usuario = {
+      name: 'Teste',
+      email: 'teste@teste.com',
+      password: 'teste123',
+      nomeUsuario: 'Teste',
+      telefone: '1234567890',
+      cpf: '12345678901',
+      endereco: 'Rua Teste, 123',
+      funcao: Funcao.Admin 
+    };
+    try {
+      await service.registrarNovoUsuario(email, password, usuario);
+      fail('Deveria ter falhado');
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+
+
+it('deve registrar um novo usuário com sucesso', async () => {
+  const email = 'novo-usuario@teste.com';
+  const password = 'teste123';
+  const usuario: Usuario = {
+    name: 'Novo Usuário',
+    email: 'novo-usuario@teste.com',
+    password: 'teste123',
+    nomeUsuario: 'Novo Usuário',
+    telefone: '1234567890',
+    cpf: '12345678901',
+    endereco: 'Rua Teste, 123',
+    funcao: Funcao.Admin 
+  };
+  try {
+    await service.registrarNovoUsuario(email, password, usuario);
+    expect(service.utilizadorAtual$).toBeTruthy();
+  } catch (error) {
+    console.error('Erro ao registrar novo usuário:', error);
+  }
+});
+
+
+
+  it('deve obter a função do usuário corretamente', async () => {
+    const funcao = await service.obter_funcao_usuario();
+    expect(funcao).toBeTruthy();
+  });
+
+ 
+  it('deve obter o usuário atual corretamente', async () => {
+    await service.login('teste@teste.com', 'teste123');
+    const usuario = await service.obterUsuarioAtual();
+    expect(usuario).toBeTruthy();
+    await service.logout();
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    const usuarioAtual = await service.obterUsuarioAtual();
+    expect(usuarioAtual).toBeNull();
+  });
+
+
+  it('deve fazer logout com sucesso', async () => {
+    await service.login('teste@teste.com', 'teste123');
+    expect(service.utilizadorAtual$).toBeTruthy();
+    await service.logout();
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    console.log('Usuário atual após logout:', service.utilizadorAtual$);
+    service.utilizadorAtual$.subscribe(usuarioAtual => {
+      console.log('Usuário atual após logout:', usuarioAtual);
+      expect(usuarioAtual).toBeNull();
+    });
+  });
+ 
 });
--- src/app/servicos/livro.service.spec.ts
+++ src/app/servicos/livro.service.spec.ts
@@ -1,16 +1,93 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LivroService } from './livro.service';
+import { Firestore, collection, getDocs, query} from '@angular/fire/firestore';
+import { Comentario } from '../componentes/comentario';
+import { User } from 'firebase/auth';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 describe('LivroService', () => {
   let service: LivroService;
+  let firestore: Firestore;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        provideFirebaseApp(() => initializeApp({
+          apiKey: 'AIzaSyAUKmMzSc-uBwwm-s3HZua6ptlQSyN6vIg',
+          authDomain: 'autent-5db11.firebaseapp.com',
+          databaseURL: 'https://autent-5db11-default-rtdb.firebaseio.com',
+          projectId: 'autent-5db11',
+          storageBucket: 'autent-5db11.appspot.com',
+          messagingSenderId: '1018635080099',
+        })),
+        provideFirestore(() => getFirestore())
+      ],
+      providers: [
+        LivroService
+      ]
+    });
     service = TestBed.inject(LivroService);
+    firestore = TestBed.inject(Firestore);
   });
 
-  it('should be created', () => {
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deve adicionar um novo comentário', async () => {
+    const usuario: User = {
+      uid: '1234567890',
+      email: 'teste@teste.com',
+      displayName: 'Nome do usuário',
+      phoneNumber: '1234567890',
+      providerId: 'providerId',
+      photoURL: 'https://example.com/foto.jpg',
+      emailVerified: false,
+      isAnonymous: false,
+      metadata: {
+        creationTime: '2022-01-01T00:00:00.000Z',
+        lastSignInTime: '2022-01-01T00:00:00.000Z'
+      },
+      providerData: [
+        {
+          displayName: 'Nome do usuário',
+          phoneNumber: '1234567890',
+          photoURL: 'https://example.com/foto.jpg',
+          providerId: 'providerId',
+          email: 'teste@teste.com', 
+          uid: '1234567890'
+        }
+      ],
+      refreshToken: 'refreshToken',
+      toJSON: () => ({}),
+      tenantId: null,
+      delete: () => Promise.resolve(),
+      getIdToken: () => Promise.resolve(''),
+      getIdTokenResult: () => Promise.resolve({
+        token: '',
+        authTime: '',
+        expirationTime: '',
+        issuedAtTime: '',
+        signInProvider: '',
+        signInSecondFactor: '',
+        signInProviderId: '',
+        claims: {}
+      }),
+      reload: () => Promise.resolve()
+    };
+  
+    const comentario: Comentario = {
+      userId: usuario.uid,
+      texto: 'Comentário teste',
+      data: '2022-01-01',
+      livroId: '1234567890'
+    };
+  
+    await service.adicionarComentario(usuario, comentario.livroId, comentario.texto);
+    const comentarioColecao = collection(firestore, 'comentarios');
+    const q = query(comentarioColecao);
+    const snapshot = await getDocs(q);
+    expect(snapshot.docs.length).toBeGreaterThan(0);
   });
 });
